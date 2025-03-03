c232dfff9e8a0c602f204379057e2d2817cfb52b
Matej Brezničan <148859673+UnizaMatBre@users.noreply.github.com>
2025-03-03 16:28:16 +0100

    feat: Added empty function and its documentation

--- source/Algorithms/Querying.h
+++ source/Algorithms/Querying.h
@@ -2,6 +2,23 @@
 #define QUERYING_H
 
 namespace Algorithms {
+	/**
+	*
+	* Gets values from source collection and puts those that satisfy selector into target colection
+	*
+	* \tparam InputIterType : iterator type of collection from which function gets items
+	* \tparam OutputIterType : iterator type of collection into which function puts items
+	* \tparam UnaryOperation : callable type used to select items
+	*
+	* \param sourceBegin : iterator pointint to the begin of the source collection
+	* \param sourceEnd : iterator pointing to the end of the source collection
+	* \param target : iterator pointing to the collection where we want to put selected items.
+	* \param selector : callable object which select valid items. It has one parameter
+	*/
+	template<typename InputIterType, typename OutputIterType, typename UnaryOperation>
+	void select(InputIterType& sourceStart, InputIterType& sourceEnd, OutputIterType& target, UnaryOperation selector) {
+
+	}
 
 }
 
