3820150e4f8a32d31e07a4afb2ae8ad954784cfa
Matej Brezničan <148859673+UnizaMatBre@users.noreply.github.com>
2025-03-03 16:37:22 +0100

    doc: Corrected spelling and names of params

--- source/Algorithms/Querying.h
+++ source/Algorithms/Querying.h
@@ -13,13 +13,13 @@ namespace Algorithms {
 	* \tparam OutputIterType : iterator type of collection into which function puts items
 	* \tparam UnaryOperation : callable type used to select items
 	*
-	* \param sourceBegin : iterator pointint to the begin of the source collection
+	* \param sourceStart : iterator pointing to the beginning of the source collection
 	* \param sourceEnd : iterator pointing to the end of the source collection
-	* \param target : iterator pointing to the collection where we want to put selected items.
+	* \param targetCurrent : iterator pointing to the collection where we want to put selected items.
 	* \param selector : callable object which select valid items. It has one parameter
 	*/
 	template<typename InputIterType, typename OutputIterType, typename UnaryOperation>
-	void select(InputIterType& sourceStart, InputIterType& sourceEnd, OutputIterType& target, UnaryOperation selector) {
+	void select(InputIterType& sourceStart, InputIterType& sourceEnd, OutputIterType& targetCurrent, UnaryOperation selector) {
 		if(sourceEnd - sourceStart < 0) {
 			throw std::invalid_argument("Source start and end don't create a valid range");
 		}
@@ -28,8 +28,8 @@ namespace Algorithms {
 			typename std::iterator_traits<InputIterType>::value_type item = *sourceStart;
 
 			if(selector(item)) {
-				*target = item;
-				++target;
+				*targetCurrent = item;
+				++targetCurrent;
 			}
 
 			++sourceStart;
