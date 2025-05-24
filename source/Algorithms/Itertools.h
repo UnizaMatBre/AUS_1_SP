589e6a54cf549f0d357dc5a38647489dc865f97e
Matej Brezničan <148859673+UnizaMatBre@users.noreply.github.com>
2025-05-24 22:48:35 +0200

    feat: Added iterator swapper

--- source/Algorithms/Itertools.h
+++ source/Algorithms/Itertools.h
@@ -1,4 +1,21 @@
 #ifndef ITERTOOLS_H
 #define ITERTOOLS_H
 
+namespace Algorithms {
+	/**
+	 * Swaps values accessed in passed iterators
+	 *
+	 * \param iter1 first iterator whose value will be swaped
+	 * \param iter2 second iterator whose value will be swaped
+	 */
+	template<typename IterType1, typename IterType2>
+	void swap_iterators(IterType1 iter1, IterType2 iter2) {
+		auto tmp = *iter1;
+		*iter1 = *iter2;
+		*iter2 = tmp;
+	}
+
+}
+
+
 #endif //ITERTOOLS_H
