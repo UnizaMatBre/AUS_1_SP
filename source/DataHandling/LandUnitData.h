a4e5efeee1f8941350663074ff977fd33c254933
Matej Brezničan <148859673+UnizaMatBre@users.noreply.github.com>
2025-05-24 20:52:16 +0200

    feat: Added basic functionality and constructor

--- source/DataHandling/LandUnitData.h
+++ source/DataHandling/LandUnitData.h
@@ -1,8 +1,26 @@
 #ifndef LANDUNITDATA_H
 #define LANDUNITDATA_H
 
+#include <string>
+
 namespace DataHandling {
-	class LandUnitData {};
+	const size_t LAND_UNIT_POPULATION_COUNT = 5;
+
+	class LandUnitData {
+		std::string name_;
+		std::string identifier_;
+		int territory = -1;
+
+		int male_population_count_[LAND_UNIT_POPULATION_COUNT] = {};
+		int female_population_count_[LAND_UNIT_POPULATION_COUNT] = {};
+
+	public:
+		LandUnitData(const std::string &name, const std::string& identifier, const int territory) {
+			this->name_ = name;
+			this->identifier_ = identifier;
+			this->territory = territory;
+		};
+	};
 }
 
 
