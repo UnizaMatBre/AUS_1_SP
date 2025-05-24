32ab59cb0b8f853561417f509cb3744a90592f9b
Matej Brezničan <148859673+UnizaMatBre@users.noreply.github.com>
2025-05-24 22:36:17 +0200

    feat: Added predicates for Min and Max population

--- source/Algorithms/Predicates.cpp
+++ source/Algorithms/Predicates.cpp
@@ -5,3 +5,22 @@ bool Algorithms::ContainsSubstringInName::operator()(DataHandling::LandUnitData&
 	return landUnitData.get_name().find(this->substring_) != std::string::npos;
 }
 
+
+Algorithms::HasMaxResidents Algorithms::HasMaxResidents::InYear(const size_t year, const int limit_) {
+	return HasMaxResidents(2020 - year, limit_);
+}
+
+bool Algorithms::HasMaxResidents::operator()(const DataHandling::LandUnitData& landUnitData) const {
+	return landUnitData.get_total_population_at(this->index_) <= this->limit_;
+}
+
+
+
+
+Algorithms::HasMinResidents Algorithms::HasMinResidents::InYear(const size_t year, const int limit_) {
+	return HasMinResidents(2020 - year, limit_);
+}
+
+bool Algorithms::HasMinResidents::operator()(const DataHandling::LandUnitData& landUnitData) const {
+	return landUnitData.get_total_population_at(this->index_) >= this->limit_;
+}
--- source/Algorithms/Predicates.h
+++ source/Algorithms/Predicates.h
@@ -14,6 +14,32 @@ namespace Algorithms {
 		explicit ContainsSubstringInName(const std::string& substring) : substring_(substring) {}
 		bool operator()(DataHandling::LandUnitData& landUnitData) const;
 	};
+
+
+
+	class HasMaxResidents {
+		const size_t index_;
+		const int limit_;
+
+	public:
+		static HasMaxResidents InYear(const size_t year, const int limit_);
+
+		explicit HasMaxResidents(const size_t index, const int limit) : index_(index), limit_(limit) {}
+		bool operator()(const DataHandling::LandUnitData& landUnitData) const;
+	};
+
+
+
+	class HasMinResidents {
+		const size_t index_;
+		const int limit_;
+
+	public:
+		static HasMinResidents InYear(const size_t year, const int limit_);
+
+		explicit HasMinResidents(const size_t index, const int limit) : index_(index), limit_(limit) {}
+		bool operator()(const DataHandling::LandUnitData& landUnitData) const;
+	};
 }
 
 #endif //PREDICATES_H
