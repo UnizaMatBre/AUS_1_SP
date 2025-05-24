c1e7a13bccad53c21674d70a9995d8b42721c9b9
Matej Brezničan <148859673+UnizaMatBre@users.noreply.github.com>
2025-05-24 23:55:44 +0200

    feat: Added population comparator

--- source/Algorithms/Comparators.cpp
+++ source/Algorithms/Comparators.cpp
@@ -1,5 +1,26 @@
 #include "Comparators.h"
 
+#include <stdexcept>
+
 int Algorithms::CompareAlphabetical(const DataHandling::LandUnitData &left, const DataHandling::LandUnitData &right) {
 	return left.get_name().compare(right.get_name());
 };
+
+
+bool Algorithms::ComparePopulation::operator()(const DataHandling::LandUnitData& left, const DataHandling::LandUnitData& right) const {
+	switch (this->category_) {
+		case ComparePopulation::Category::Male: {
+			return left.get_male_population_at(this->index_) - right.get_male_population_at(this->index_);
+		};
+
+		case ComparePopulation::Category::Female: {
+			return left.get_female_population_at(this->index_) - right.get_female_population_at(this->index_);
+		};
+		case ComparePopulation::Category::Both: {
+			return left.get_total_population_at(this->index_) - right.get_total_population_at(this->index_);
+		}
+		default: {
+			throw std::invalid_argument("Unexpected category.");
+		};
+	}
+}
--- source/Algorithms/Comparators.h
+++ source/Algorithms/Comparators.h
@@ -7,6 +7,25 @@ namespace Algorithms {
 	int CompareAlphabetical(const DataHandling::LandUnitData& left, const DataHandling::LandUnitData& right);
 
 
+	/**
+	 * Represents comparator that compares two units by population in specific year.
+	 */
+	class ComparePopulation {
+	public:
+		enum class Category {Male = 0, Female = 1, Both = 2};
+
+	private:
+		const size_t index_;
+		const Category category_;
+
+	public:
+		static ComparePopulation InYear(const size_t year, const Category category) {
+			return {2020 - year, category};
+		};
+		ComparePopulation(const size_t index, const Category category) : index_(index), category_(category) {};
+		bool operator()(const DataHandling::LandUnitData& left, const DataHandling::LandUnitData& right) const;
+	};
+
 };
 
 #endif //COMPARATORS_H
