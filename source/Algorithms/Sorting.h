955984fca2c31a4c497fa616d2247aa31dbec8a5
Matej Brezničan <148859673+UnizaMatBre@users.noreply.github.com>
2025-05-25 17:23:35 +0200

    feat: Removed need for subtraction operator from quick_sort

--- source/Algorithms/Sorting.h
+++ source/Algorithms/Sorting.h
@@ -55,6 +55,10 @@ namespace Algorithms {
 		// this will be the value that will be swaped around
 		SortedIterType middle = (start + 1);
 
+		// this is iter that is always behind middle
+		// why? So that the iterator is not forced to implement subtraction operator
+		SortedIterType prev_middle = start;
+
 		// iterate from middle to end (we don't want to iterate from pivot)
 		for (auto iter = middle; iter < end; ++iter) {
 			// is current iterator less than pivot?
@@ -63,6 +67,9 @@ namespace Algorithms {
 				// swap current value and 'middle'
 				Algorithms::swap_iterators(iter, middle);
 
+				// store middle in prev_mide
+				prev_middle = middle;
+
 				// move middle to next element
 				++middle;
 			}
@@ -70,7 +77,7 @@ namespace Algorithms {
 		// swap pivot with value PREVIOUS to the middle
 		// this is done because middle contains value LARGER than pivot and swaping with that would break it
 		// but value BEFORE middle is smaller than pivot - that is because smaller value is swaped into place of previous middle
-		Algorithms::swap_iterators(pivot, middle - 1);
+		Algorithms::swap_iterators(pivot, prev_middle);
 
 
 		/*
@@ -81,7 +88,7 @@ namespace Algorithms {
 		*/
 
 		// call quicksort on left and right part
-		Algorithms::quick_sort(start, middle - 1, comparator);
+		Algorithms::quick_sort(start, prev_middle, comparator);
 		Algorithms::quick_sort(middle, end, comparator);
 	}
 };
