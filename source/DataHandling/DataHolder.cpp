bd50a9f0b23eb70f2489b2a95b8c1b8672811ce2
Matej Brezničan <148859673+UnizaMatBre@users.noreply.github.com>
2025-05-25 02:27:30 +0200

    feat: Implemented universal mapper

--- source/DataHandling/DataHolder.cpp
+++ source/DataHandling/DataHolder.cpp
@@ -16,57 +16,28 @@ DataHandling::DataHolder::DataHolder() {
 	*/
 
 	// STEP 1 (ONE)
-
 	// initialize temporary table
 	Containers::LinkedTable<std::string, LandNodeType*> id_to_node_mapper;
 
+	// add austria into this temporary table
+	id_to_node_mapper.insert("AT", &this->root_node_);
 
-	// load geographic areas
+	// load upper areas
 	{
+		// open stream
 		auto stream = std::ifstream(R"(../../data/geograficke_oblasti.csv)");
 		std::string line;
 
-		while (getline(stream, line)) {
-
-			std::string name, full_id, restricted_id;
-
-			// read data
-			auto csv_reader = DataHandling::CsvLineReader(line);
-			csv_reader
-				.handleField([&name](const std::string& field ) {
-					name = field;
-				})
-			    ->handleField([&full_id, &restricted_id](const std::string& field ) {
-					full_id = field;
-			    	restricted_id = field.substr(1, field.size() - 2);
-				});
-
-			auto land_unit_ptr = &this->land_units_list_.push_back({name, full_id, 1});
-
-			// insert into geographic area table
-			this->geographic_areas_table_.insert(name, land_unit_ptr);
-
-			// create new node
-			auto new_node_ptr = this->root_node_.push_back_children(land_unit_ptr);
-
-			// insert this new node into mapping table
-			id_to_node_mapper.insert(restricted_id, new_node_ptr);
+		if (not stream.is_open()) {
+			throw std::runtime_error("Could not open file");
 		};
 
-	}
-
-	// load republics
-	{
-		auto stream = std::ifstream(R"(../../data/republiky.csv)");
-		std::string line;
-
-		while (getline(stream, line)) {
-
+		// cycle over every line in file
+		while (std::getline(stream, line)) {
 			std::string name, full_id, restricted_id;
 
 			// read data
-			auto csv_reader = DataHandling::CsvLineReader(line);
-			csv_reader
+			DataHandling::CsvLineReader(line)
 				.handleField([&name](const std::string& field ) {
 					name = field;
 				})
@@ -75,22 +46,39 @@ DataHandling::DataHolder::DataHolder() {
 					restricted_id = field.substr(1, field.size() - 2);
 				});
 
-			auto land_unit_ptr = &this->land_units_list_.push_back({name, full_id, 1});
-
-			// insert into geographic area table
-			this->republics_table_.insert(name, land_unit_ptr);
+			// create parent's restricted id - this is achievable by the fact that parent id is always shorter by one digit from child
+			std::string restricted_parent_id = restricted_id.substr(0, restricted_id.size() - 1);
 
 			// get parent node
-			std::string parent_restricted_id = restricted_id.substr(0, restricted_id.size() - 1);
-			auto parent_node = id_to_node_mapper.at(parent_restricted_id);
+			auto parent_node_ptr = id_to_node_mapper.at(restricted_parent_id);
 
-			// create new node
-			auto new_node_ptr = parent_node->push_back_children(land_unit_ptr);
+			// create new land unit
+			auto new_land_unit_ptr = &this->land_units_list_.push_back(
+				{name, full_id, parent_node_ptr->get_item()->get_unit_level() + 1}
+			);
 
-			// insert this new node into mapping table
-			id_to_node_mapper.insert(restricted_id, new_node_ptr);
-		};
+			// create new tree node
+			auto new_land_node_ptr =  parent_node_ptr->push_back_children(new_land_unit_ptr);
 
-	}
+			// insert new land unit into correct table
+			switch (new_land_unit_ptr->get_unit_level()) {
+				case 1:
+					this->geographic_areas_table_.insert(name, new_land_unit_ptr);
+					break;
+				case 2:
+					this->republics_table_.insert(name, new_land_unit_ptr);
+					break;
+				case 3:
+					this->regions_table_.insert(name, new_land_unit_ptr);
+					break;
+				default:
+					throw std::runtime_error("This is not supposed to happend");
+			}
 
+			// insert land node into mapper
+			id_to_node_mapper.insert(restricted_id, new_land_node_ptr);
+		}
+
+		stream.close();
+	};
 }
