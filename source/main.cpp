b7721445a9dd58f1a1564e1cbe9511dd3a572e5f
Matej Brezničan <148859673+UnizaMatBre@users.noreply.github.com>
2025-05-25 02:41:03 +0200

    feat: Removed everything from main

--- source/main.cpp
+++ source/main.cpp
@@ -1,19 +1,5 @@
-#include <fstream>
-#include <string>
-#include <vector>
-#include <fstream>
-#include <iostream>
-#include <optional>
-
-#include "Algorithms/Querying.h"
-#include "Algorithms/Sorting.h"
-#include "DataHandling/TownData.h"
-#include "DataHandling/DataHolder.h"
-
-#include "Containers/LinkedList.h"
-#include "Containers/NodeBasedTree.h"
-
 
 int main() {
+
 	return 0;
 }
