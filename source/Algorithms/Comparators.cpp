94337a86c27c4a2dd664a37a5e354baa9f6f3eb5
Matej Brezničan <148859673+UnizaMatBre@users.noreply.github.com>
2025-05-25 04:30:29 +0200

    feat: Turned access to populations into true indexers

--- source/Algorithms/Comparators.cpp
+++ source/Algorithms/Comparators.cpp
@@ -10,11 +10,11 @@ int Algorithms::CompareAlphabetical(const DataHandling::LandUnitData &left, cons
 bool Algorithms::ComparePopulation::operator()(const DataHandling::LandUnitData& left, const DataHandling::LandUnitData& right) const {
 	switch (this->category_) {
 		case ComparePopulation::Category::Male: {
-			return left.get_male_population_at(this->index_) - right.get_male_population_at(this->index_);
+			return left.male_population_at(this->index_) - right.male_population_at(this->index_);
 		};
 
 		case ComparePopulation::Category::Female: {
-			return left.get_female_population_at(this->index_) - right.get_female_population_at(this->index_);
+			return left.female_population_at(this->index_) - right.female_population_at(this->index_);
 		};
 		case ComparePopulation::Category::Both: {
 			return left.get_total_population_at(this->index_) - right.get_total_population_at(this->index_);
